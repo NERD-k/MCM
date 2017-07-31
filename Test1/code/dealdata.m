af5df0b029c61e500cb9a6ed2fff622a1167a9aa
NERD <smallnerd.k@gmail.com>
2017-08-01 01:51:06 +0800

    Add data_deal Mcode

---
+++ Test1/code/dealdata.m
@@ -0,0 +1,47 @@
+clc, clear all
+
+%%  对数据的处理
+
+% 读取文件
+% 如要修改，将origin.xlsx文件路径填入，或者放在此m文件相同目录下，使用xlsread('origin.xlsx')
+data = xlsread('D:\Mcm\Test1\origin.xlsx');
+
+% 上车下车人数分别存储在矩阵 up 和 down 中
+for i = 1:2:36
+    up((i+1)/2,:) = data(i,:);
+end
+for i = 2:2:36
+    down(i/2,:) = data(i,:);
+end
+
+% 每站上车人数与下车人数之差
+A = up - down;
+% 累积求和
+B = cumsum(A');
+B = B';
+
+
+% 相邻时间段的人数差
+temp = A;
+temp = cat(1,zeros(1,14),A);
+temp(15,:) = [];
+C = A - temp;
+
+% 每个时间段的总净增加人数
+D = B(:,14);
+
+% 每个时间段的人数
+E = cumsum(D);
+
+
+F = cumsum(A);
+
+% 文件写入
+% 如要修改，将deal1.xlsx文件路径填入，或者放在此m文件相同目录下，使用xlswrite('deal1.xlsx')
+delete('D:\Mcm\Test1\deal1.xlsx')  % 覆盖写入
+xlswrite('D:\Mcm\Test1\deal1.xlsx', A, 'A')
+xlswrite('D:\Mcm\Test1\deal1.xlsx', B', 'B')
+xlswrite('D:\Mcm\Test1\deal1.xlsx', C, 'C')
+xlswrite('D:\Mcm\Test1\deal1.xlsx', D, 'D')
+xlswrite('D:\Mcm\Test1\deal1.xlsx', E, 'E')
+xlswrite('D:\Mcm\Test1\deal1.xlsx', F, 'F')
