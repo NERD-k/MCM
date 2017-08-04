658f70176fa1c679e24cfb023daded0d2398782f
NERD <smallnerd.k@gmail.com>
2017-08-05 00:45:49 +0800

    deal with data, get all grade sheet

---
+++ Test2/Code/demo.m
@@ -0,0 +1,9 @@
+% ×¢ÊÍ
+
+clc,clear
+
+load('red1.mat','red1');
+load('red2.mat','red2');
+load('write1.mat','write1');
+load('write2.mat','write2');
+
---
+++ Test2/Code/predata.m
@@ -0,0 +1,7 @@
+% ×¢ÊÍ
+load('red1new.mat')
+load('red2new.mat')
+load('write1new.mat')
+load('write2new.mat')
+
+
