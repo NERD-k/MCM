6614b846bb1b8ca0ff46741a2e65948b7b1a4616
NERD <smallnerd.k@gmail.com>
2017-08-16 02:09:56 +0800

    Make code better

--- Test4/Code/Q21.m
+++ Test4/Code/Q21.m
@@ -3,27 +3,27 @@ clc,clear
 %% 已知参数
 g = 9.8;
 rho = 1025;                     % 海水密度
-l = (1 - 2 / 5) * 0.105;
-N = 22.05 / ((1 - 2/5) * 0.105);
+l = (1 - 2 / 5) * 0.105;        % 链节的有效长度
+N = 22.05 / ((1 - 2/5) * 0.105);% 链节最大数量
 m1 = 1000;                      % 浮标的质量
 m2 = 10;                        % 每节钢管的质量
 m3 = 100;                       % 钢桶的质量
-m4 = 1200;                      % 铁球的质量
+m4 = 1200;                      % 重物球的质量
 m5 = 22.05 * 7 / N;             % 锚链链结的质量
-m6 = 600;
+m6 = 600;                       % 锚的质量
 R_fubiao = 2 / 2;               % 浮标的半径
-R_tieguan = 0.05 / 2;           % 铁管的半径
-R_tietong = 0.3 / 2;            % 铁桶的半径
-
+R_tieguan = 0.05 / 2;           % 钢管的半径
+R_tietong = 0.3 / 2;            % 钢桶的半径
 
 F_tieguan = rho * pi * R_tieguan^2 * 1;      % 钢管的浮力
-F_tietong = rho * pi * R_tietong^2 * 1;      % 铁桶的浮力
+F_tietong = rho * pi * R_tietong^2 * 1;      % 钢桶的浮力
 
+%% 待定参数
 v = 36;
 h = 0.7513916;      % 初始吃水深度
 %% 受力分析
-F_feng = 0.625 * R_fubiao * (2 - h) * v^2;     % 近海风荷载
-F_fubiao = rho * pi * R_fubiao^2 * h;      % 浮标的浮力
+F_feng = 0.625 * R_fubiao * (2 - h) * v^2;    % 近海风荷载
+F_fubiao = rho * pi * R_fubiao^2 * h;         % 浮标的浮力
 % 上半部分受力分析
 a = zeros(6,1);
 a(1,1) = g * (F_fubiao - m1);
@@ -62,10 +62,16 @@ youdong = l * (N - n) + sum(sind(alpha(2:6,1))) + sum(l * sind(theta));
 
 % 锚链形状
 % 横坐标
+temp = [0;l * (N - n)];
 x = cumsum(l * sind(theta(end:-1:1)));
+x = cat(1,temp,x);
 % 纵坐标
+temp = [0;0];
 y = cumsum(l * cosd(theta(end:-1:1)));
+y = cat(1,temp,y);
 % 画图
 figure
 plot(x,y,'r-');
+hold on
+plot(x(2),y(2),'b*');    % 锚链恰好脱离海床的位置
 title('风速为36m/s时锚链的形状');
